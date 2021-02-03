33c44325d18d0ef57ba546401f61373b70d7a9a7
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-03 11:44:14 -0800

    Add good fail of class userDate.
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

---
+++ __tests__/date.test.js
@@ -0,0 +1,10 @@
+import userDate from './../src/js/date.js';
+
+describe('userDate', () => {
+  test('should correctly create a date object', () => {
+    const date = new userDate(11,24,1988);
+    expect(date.month).toEqual(11);
+    expect(date.day).toEqual(24);
+    expect(date.year).toEqual(1988);
+  });
+});
