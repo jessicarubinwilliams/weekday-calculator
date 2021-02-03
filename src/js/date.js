145161984f7babb2972f9bb038117f40ef389ee5
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-03 13:11:52 -0800

    Add code block to userDate constructor.
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- src/js/date.js
+++ src/js/date.js
@@ -1,12 +1,10 @@
 export default class userDate {
   constructor(month,day,year) {
-
+    {
+      this.month = month;
+      this.day = day;
+      this.year = year;
+    }
   }
 };
 
-
-  // {
-  //   this.month = month;
-  //   this.day = day;
-  //   this.year = year;
-  // }
