a17d2bfd925961c3f0592c5823a62deb0c0a3644
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-04 10:35:42 -0800

    Update title.
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Document',
+      title: 'Weekday Calculator',
       template: './src/index.html',
       inject: 'body'
     })
