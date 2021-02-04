c2055122764b0b7aa1b7e5358ae9b81357efc4bd
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-04 10:31:34 -0800

    Update TDD1 findDay().
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- __tests__/date.test.js
+++ __tests__/date.test.js
@@ -1,14 +1,24 @@
-import userDate from './../src/js/date.js';
+import findDay from './../src/js/date.js';
 
-describe('userDate', () => {
-  test('should correctly create a date object', () => {
-    const date = new userDate(11,24,1988);
-    expect(date.month).toEqual(11);
-    expect(date.day).toEqual(24);
-    expect(date.year).toEqual(1988);
-  });
-  test('should determine day of the week of date object', () => {
-    const date = new userDate(11,24,1988);
-    expect(date.findDay()).toEqual("Thursday");
+describe('findDay', () => {
+
+  test('should determine day of the week of date', () => {
+    const testDate = new Date("1 31, 2021");
+    expect(findDay(testDate)).toEqual("Sunday");
+    const testDate2 = new Date("2 1, 2021");
+    expect(findDay(testDate2)).toEqual("Monday");
+    const testDate3 = new Date("2 2, 2021");
+    expect(findDay(testDate3)).toEqual("Tuesday");
+    const testDate4 = new Date("2 3, 2021");
+    expect(findDay(testDate4)).toEqual("Wednesday");
+    const testDate5 = new Date("2 4, 2021");
+    expect(findDay(testDate5)).toEqual("Thursday");
+    const testDate6 = new Date("2 5, 2021");
+    expect(findDay(testDate6)).toEqual("Friday");
+    const testDate7 = new Date("2 6, 2021");
+    expect(findDay(testDate7)).toEqual("Saturday");
+    const testDate8 = new Date("not a date");
+    expect(findDay(testDate8)).toEqual("Entry is not a date");
+    
   });
 });
