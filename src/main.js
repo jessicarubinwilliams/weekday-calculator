7e0b1ef12de25ee6174878e71269a9fd4d54f325
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-04 11:33:04 -0800

    import findDay(), add UIL.
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,3 +2,11 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import findDay from './js/date.js';
+
+$("#formOne").submit(function(event) {
+  event.preventDefault();
+  let userInput = new Date($("input#userDate").val());
+  $(".output").show();
+  $("#dayOutput").text(findDay(userInput));
+});
