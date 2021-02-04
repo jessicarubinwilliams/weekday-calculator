5cda6f856e661215439bc8aa17803c4fcf6a5c82
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-04 11:45:36 -0800

    Dynamically output day.
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- src/main.js
+++ src/main.js
@@ -6,7 +6,9 @@ import findDay from './js/date.js';
 
 $("#formOne").submit(function(event) {
   event.preventDefault();
+  let htmlForOutput = "<p>The day of the week is <span id='dayOutput'></span>.</p>";
   let userInput = new Date($("input#userDate").val());
   $(".output").show();
+  $(".output").append(htmlForOutput);
   $("#dayOutput").text(findDay(userInput));
 });
