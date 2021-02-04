205831352747f5b2c335ff960e2b4e696baae79a
Jessica R. Williams <jessicarubinwilliams@gmail.com>
2021-02-04 10:33:37 -0800

    Remove constructor add function.
    Co-authored-by: Dani Renner <danijrenner@gmail.com>

--- src/js/date.js
+++ src/js/date.js
@@ -1,13 +1,27 @@
-export default class userDate {
-  constructor(month,day,year) {
-    {
-      this.month = month;
-      this.day = day;
-      this.year = year;
-    }
+export default function findDay(userDate) {
+  userDate = userDate.getDay();
+  if (userDate === 0) {
+    return "Sunday";
   }
-  findDay() {
-    
+  else if (userDate === 1){
+    return "Monday";
+  }
+  else if (userDate === 2){
+    return "Tuesday";
+  }
+  else if (userDate === 3){
+    return "Wednesday";
+  }
+  else if (userDate === 4){
+    return "Thursday";
+  }
+  else if (userDate === 5){
+    return "Friday";
+  }
+  else if (userDate === 6){
+    return "Saturday";
+  }
+  else {
+    return "Entry is not a date";
   }
 };
-
